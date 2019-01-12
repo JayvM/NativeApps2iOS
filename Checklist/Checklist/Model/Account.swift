760ce5f3c4e5fa78d8d08724037c33e4fc06c1ff
Jay van Mook <jay.vanmook.y7524@student.hogent.be>
2019-01-12 15:11:58 +0100

    Creating checklists screen + coding segue

--- Checklist/Checklist/Model/Account.swift
+++ Checklist/Checklist/Model/Account.swift
@@ -4,6 +4,7 @@ class Account: Codable {
     
     //Properties
     
+    var name: String
     var email: String
     var password: String
     
@@ -14,7 +15,8 @@ class Account: Codable {
 
     //Initializer
     
-    init(email: String, password: String) {
+    init(name: String, email: String, password: String) {
+        self.name = name
         self.email = email
         self.password = password
     }
--- Checklist/Checklist/View Controllers/LoginViewController.swift
+++ Checklist/Checklist/View Controllers/LoginViewController.swift
@@ -24,7 +24,19 @@ class LoginViewController: UIViewController {
         if let accounts = Account.getAccounts() {
             self.accounts = accounts
         } else {
-            accounts.append(Account(email: "test@mail.com", password: "test"))
+            accounts.append(Account(name: "Test", email: "test@mail.com", password: "test"))
+        }
+    }
+    
+    /*
+     Stack Overflow: Passing data from a view controller to a tableview controller through a navigation controller
+     https://stackoverflow.com/questions/25369412/swift-pass-data-through-navigation-controller
+    */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ChecklistsSegue" {
+            let navigationController = segue.destination as? UINavigationController
+            let checklistsTableViewController = navigationController?.viewControllers.first as! ChecklistsTableViewController
+            checklistsTableViewController.account = sender as? Account
         }
     }
     
@@ -47,7 +59,7 @@ class LoginViewController: UIViewController {
         
         for account in accounts {
             if account.email == email && account.password == password {
-                print("logged in")
+                performSegue(withIdentifier: "ChecklistsSegue", sender: account)
                 return
             }
         }
